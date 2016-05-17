81b1f3f076b602479add837c8cfdfd88933a1314
Treyzania <Treyzania@gmail.com>
2016-05-17 01:12:06 -0400

    Cards get positions in a more readable and prettier way.

--- Assets/Code/CardHand.cs
+++ Assets/Code/CardHand.cs
@@ -11,7 +11,31 @@ namespace NorthmenGoFish.Unity {
 		public Transform discardTransform;
 		
 		public float cardSpacing;
-		
+		public float backwardsSpacing;
+		public float upSpacing;
+
+		public int UniqueValues {
+
+			get {
+
+				List<CardValue> valsEncountered = new List<CardValue>();
+
+				foreach (CardController cc in this.cards) {
+
+					CardValue cv = cc.cardType.Value;
+
+					if (!valsEncountered.Contains(cv)) {
+						valsEncountered.Add(cv);
+					}
+
+				}
+
+				return valsEncountered.Count;
+
+			}
+
+		}
+
 		void Start () {
 			
 			foreach (CardController cc in cards) {
@@ -19,23 +43,64 @@ namespace NorthmenGoFish.Unity {
 			}
 			
 		}
-		
-		void Update () {
-			
-		}
-		
+
 		public Vector3 GetCardPosition(CardController cc) {
 			
-			if (!this.cards.Contains (cc)) return Vector3.zero;
-			int index = this.cards.FindIndex(o => cc == o);
+			if (!this.cards.Contains(cc)) return Vector3.zero;
+			int sideIndex = this.GetCardValueIndex(cc.cardType.Value);
+			int backIndex = this.GetPreviousCardsOfValueInDeck(cc.cardType);
 			
-			Vector3 increment = this.transform.right * this.cardSpacing;
-			Vector3 minPos = this.transform.position - (increment * ((this.cards.Count - 1F) / 2F));
+			Vector3 sideIncrement = this.transform.right * this.cardSpacing;
+			Vector3 backwardsIncrement = (this.transform.forward * this.backwardsSpacing) + (this.transform.up * this.upSpacing);
+			Vector3 minPos = this.transform.position - (sideIncrement * ((this.UniqueValues - 1F) / 2F));
 			
-			return minPos + (increment * index);
+			return minPos + (sideIncrement * sideIndex) + (backwardsIncrement * backIndex);
 			
 		}
-		
+
+		private int GetCardValueIndex(CardValue val) {
+
+			List<CardValue> valsEncountered = new List<CardValue>();
+
+			foreach (CardController cc in this.cards) {
+
+				CardValue cv = cc.cardType.Value;
+
+				if (cv == val) {
+					return valsEncountered.Count;
+				} else {
+
+					// Add it to the list.
+					if (!valsEncountered.Contains(cv)) {
+						valsEncountered.Add(cv);
+					}
+
+				}
+
+			}
+
+			return -1;
+
+		}
+
+		private int GetPreviousCardsOfValueInDeck(CardType type) {
+
+			int cards = 0;
+
+			foreach (CardController cc in this.cards) {
+
+				if (cc.cardType == type) {
+					return cards;
+				} else if (cc.cardType.Value == type.Value) {
+					cards++;
+				}
+
+			}
+
+			return -1;
+
+		}
+
 		public void AddCard(CardController card) {
 			
 			this.cards.Add(card);
